09c9341a7e8b9d0ae15f4ab0a68aa3fe9720904e
Andy Wang <climber2002@gmail.com>
2017-11-12 16:16:49 +1100

    ch5

--- src/main/scala/ch5/Autobots.scala
+++ src/main/scala/ch5/Autobots.scala
@@ -2,10 +2,13 @@ package ch5
 
 import cats.data.EitherT
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import cats.implicits._
 
-trait Autobots {
+import scala.concurrent.duration.Duration
+
+object Autobots {
 
   type Response[A] = EitherT[Future, String, A]
 
@@ -18,14 +21,28 @@ trait Autobots {
   def getPowerLevel(autobot: String) = EitherT(Future {
     powerLevels.get(autobot) match {
       case Some(i) => Right(i)
-      case None => Left(s"Can't find $autobot")
+      case None => Left(s"$autobot unreachable")
     }
   })
 
   def canSpecialMove(ally1: String, ally2: String): Response[Boolean] = {
-    val result = for {
-      _ <- getPowerLevel(ally1)
-      _ <- getPowerLevel(ally2)
-    } yield true
+    for {
+      a <- getPowerLevel(ally1)
+      b <- getPowerLevel(ally2)
+    } yield if(a + b > 15) true else false
+  }
+
+  def tacticalReport(ally1: String,
+                     ally2: String
+                    ): String = {
+    val can = canSpecialMove(ally1, ally2)
+    val future = can.value.map { either =>
+      either match {
+        case Right(b) => if(b) s"$ally1 and $ally2 are ready to roll out!" else s"$ally1 and $ally2 need a recharge."
+        case Left(error) => error
+      }
+    }
+
+    Await.result(future, Duration.Inf)
   }
 }
